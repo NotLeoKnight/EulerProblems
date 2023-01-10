5287ac8875709b39b2a442a3f77ce84ef8d49b07
Leo Knight <99699893+NotLeoKnight@users.noreply.github.com>
2023-01-11 10:14:43 +1100

    Delete EulerProblems directory

--- EulerProblems/1.Multiples3-5.py
+++
@@ -1,6 +0,0 @@
-multiples = []
-for  i in range(0,1000):
-    if i % 5 == 0 or i % 3 == 0:
-        multiples.append(i)
-
-print(sum(multiples))
--- EulerProblems/6.SumSquareDifference.py
+++
@@ -1,9 +0,0 @@
-numbers = []
-for i in range(1,101):
-    numbers.append(i)
-
-numbersSqrd = []
-for i in numbers:
-    numbersSqrd.append(i**2)
-
-print(sum(numbers)**2 - sum(numbersSqrd))
